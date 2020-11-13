49f0cf7687534355717460b8ad238badbab9f0c3
dauntless-strategy-1036 <danielgordon19972@gmail.com>
2020-11-13 13:26:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,20 +9,89 @@ function setCart(c) {
   return cart;
 }
 
+function getRandomInt() {
+  let num = Math.floor(Math.random() * 100)
+  return num
+}
+
+function createObject(item) {
+  return {
+    itemName: item,
+    itemPrice: getRandomInt()
+  }
+}
+
 function addToCart(item) {
- // write your code here
+  getCart().push(createObject(item))
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  
+  let sentance = "In your cart, you have"
+  
+  if (getCart().length === 0){
+    return 'Your shopping cart is empty.'
+    
+  } else {
+  
+   for (let i = 0; getCart().length > i; i++) {
+      
+   if (getCart().length - 2 === i) {
+    sentance = sentance + ` ${getCart()[i].itemName} at $${getCart()[i].itemPrice}, and`
+    
+  } else if(getCart().length - 1 === i) {
+    sentance = sentance + ` ${getCart()[i].itemName} at $${getCart()[i].itemPrice}.`
+    
+  } else { 
+    sentance = sentance + ` ${getCart()[i].itemName} at $${getCart()[i].itemPrice},`
+   
+    }
+   }
+  } return sentance
+}
+
+function getSum() {
+  let sum = 0
+  for (let i = 0; getCart().length > i; i++) {
+    sum = sum + getCart()[i].itemPrice
+  }
+  return sum
 }
 
 function total() {
-  // write your code here
+  return getSum()
 }
 
-function removeFromCart(item) {
-  // write your code here
+function searchForItem(item) {
+  let found = false
+  for (let i = 0; getCart().length > i; i++){
+    if (getCart()[i].itemName === item) {
+    found = item
+    return found
+  } else {
+  return found
+  }
+}
+}
+
+function removeObject(item) {
+  for (let i = 0; getCart.length > i; i++) {
+    if (getCart()[i].itemName === item) {
+      return getCart().splice(i, 1)
+    }
+  }
+}
+
+
+function removeFromCart(item) { 
+  if (searchForItem(item) === false) {
+    return 'That item is not in your cart.'
+    
+  } else {
+    return removeObject(item)
+  } 
+  
 }
 
 function placeOrder(cardNumber) {
