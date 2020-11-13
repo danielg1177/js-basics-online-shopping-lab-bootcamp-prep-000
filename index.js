788dcb716f5d6dda12183cc4ff974cd6da6130e5
dauntless-strategy-1036 <danielgordon19972@gmail.com>
2020-11-13 13:48:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -91,6 +91,11 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber === '') {
-    return ''
+    return "Sorry, we don't have a credit card on file for you."
+  } else {
+    let sentance = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    getCart().splice(0)
+    return sentance
+    
   }
 }
